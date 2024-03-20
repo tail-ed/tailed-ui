87e138e2a0c41e9165aab4469140f3403b137016
Sarim <a.sarimsyed@gmail.com>
2024-03-20 14:15:14 -0400

    removed types folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,4 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-//Test to see if this file is copied over or ignored as it should be
